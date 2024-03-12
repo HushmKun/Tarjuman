53e319b477a423cea98a064ce6584fe1b18a72d9
Hussein Mukhtar <HushmKun@outlook.com>
2024-03-12 23:50:06 +0200

    test

--- Tarjuman/settings.py
+++ Tarjuman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Fuck Society"
+SECRET_KEY = "Fuck_Society_!@#$%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
