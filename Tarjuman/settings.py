b8f60b46484329ad95c7cc3756d7bff320a80f0b
Hussein Mukhtar <HushmKun@outlook.com>
2024-03-13 17:45:58 +0200

    Adheres to best practices

--- Tarjuman/settings.py
+++ Tarjuman/settings.py
@@ -11,24 +11,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Fuck_Society_!@#$%"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["tarjuman.azurewebsites.net", "127.0.0.1"]
-
-
-# Application definition
+ALLOWED_HOSTS = [".tarjuman.tech"]
 
 INSTALLED_APPS = [
     "main",
@@ -70,20 +67,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Tarjuman.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,10 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -112,18 +97,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]  # Linking Static files.
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = '/var/www/tarjuman.tech/static'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
