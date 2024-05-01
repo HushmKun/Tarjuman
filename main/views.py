4e4bd27639141ddbe4266cfce87049431ef641cd
Hussein Mukhtar <HushmKun@outlook.com>
2024-05-02 01:54:40 +0300

    Final Touches

--- main/views.py
+++ main/views.py
@@ -5,19 +5,37 @@ import random
 from django.conf import settings 
 
 
+def get_footer():
+        pharo = Post.objects.filter(category__eng_name="pharonic")
+        coptic = Post.objects.filter(category__eng_name="coptic")
+        islamic = Post.objects.filter(category__eng_name="islamic") 
+        modern = Post.objects.filter(category__eng_name="modern")
+        podcast = Post.objects.filter(category__eng_name="podcast")
+
+        return [pharo, coptic, islamic, modern, podcast]
+
+
 # Create your views here.
 
 
 def home(request):
 
-    items = Post.objects.order_by("-date")[:20]
-
+    items = Post.objects.exclude(tags__caption="مقالات").order_by("-date")[:3]
+    articles = Post.objects.filter(tags__caption="مقالات")[:5]
+    reports = Post.objects.filter(tags__caption="تقرير")[:5]
+    news = Post.objects.filter(tags__caption="أخبار")
+    static = get_footer()
     context = {
-        "main_post": items[0],
-        "smaller_posts": items[1:3],
-        "slider_posts": items[3:8],
-        "editors_choice": items[8:13],
-        "popular_stories": items[13:19],
+        "main_post": items[0] ,
+        "smaller_posts": items[1:3] if 1 < len(items) else None,
+        "slider_posts": news,
+        "editors_choice": articles,
+        "popular_stories": reports,
+        "pharo":static[0],
+        "coptic":static[1],
+        "islamic":static[2],
+        "modern":static[3],
+        "podcast":static[4]
     }
     return render(request, "main/index.html", context)
 
@@ -33,11 +51,17 @@ def category(request, cat):
     page_obj = paginator.get_page(page_number)
     page_obj.adjusted_elided_pages = paginator.get_elided_page_range(page_number)
 
+    static = get_footer()
     context = {
         "category": category,
         "categories": categories,
         "posts": posts,
         "page_obj": page_obj,
+        "pharo":static[0],
+        "coptic":static[1],
+        "islamic":static[2],
+        "modern":static[3],
+        "podcast":static[4]
     }
     return render(request, "main/category.html", context)
 
@@ -67,15 +91,29 @@ def post(request, slug):
 
     comments = Comment.objects.filter(post=post)
 
+    static = get_footer()
     context = {
         "categories": categories,
         "post": post,
         "prev_post": prev_posts,
         "next_post": next_posts,
         "comments": comments,
-        "google" : settings.GOOGLE_MAPS_API_KEY
+        "google" : settings.GOOGLE_MAPS_API_KEY,
+        "pharo":static[0],
+        "coptic":static[1],
+        "islamic":static[2],
+        "modern":static[3],
+        "podcast":static[4]
     }
     return render(request, "main/post_details.html", context)
 
 def usage(request):
-    return render(request, "main/usage.html")
+    static = get_footer()
+    context = {
+        "pharo":static[0],
+        "coptic":static[1],
+        "islamic":static[2],
+        "modern":static[3],
+        "podcast":static[4]
+    }
+    return render(request, "main/usage.html", context=context)
