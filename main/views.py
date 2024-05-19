e7473525921096e995a20b733c4a74acad18130f
Hussein Mukhtar <HushmKun@outlook.com>
2024-05-19 04:32:43 +0300

    Please

--- main/views.py
+++ main/views.py
@@ -21,7 +21,7 @@ def get_footer():
 def home(request):
 
     items = Post.objects.exclude(tags__caption="مقالات").exclude(tags__caption="إفتتاحي").order_by("-date")[:3]
-    articles = Post.objects.filter(tags__caption="مقالات")[:4].exclude(slug__contain="ynyr-drob-ltrykh-olgml-lmnsy-fy-klb-lkhr-1")
+    articles = Post.objects.filter(tags__caption="مقالات")[:4]
     reports = Post.objects.filter(tags__caption="تقرير")[:5]
     news = Post.objects.filter(tags__caption="أخبار")
     static = get_footer()
